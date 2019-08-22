84f19797435b9f19a8c1cf90e2dfd5277269e8c8
James Lee <jameswlee109@gmail.com>
2019-08-22 11:33:09 -0500

    Dog image step 2

--- index.js
+++ index.js
@@ -2,16 +2,26 @@ const getDogs = function(userInput) {
     fetch(`https://dog.ceo/api/breeds/image/random/${userInput}`)
         .then(response => response.json())
         .then(jsonData => {
-            console.log(jsonData);
+            appendDogs(jsonData)
         })
         .catch(error => console.log(error));
-}
+};
+
+const appendDogs = function(dogData){
+    dogData.message.forEach(data => {
+        $('.dog-pics').append(createTemplate(data))
+    });
+};
 
 const handleSubmit = function() {
     $('.dog-form').on('submit', event => {
         event.preventDefault();
         getDogs($('#num-dogs').val());
-    })
-}
+    });
+};
+
+const createTemplate = function(dogMessage){
+    return `<img src=${dogMessage}>`;
+};
 
 $(handleSubmit)
