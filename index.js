515e0816e6ea00dca3e9db3c0f9494a10751f596
James Lee <jameswlee109@gmail.com>
2019-08-22 13:48:44 -0500

    dog api completed

--- index.js
+++ index.js
@@ -7,31 +7,46 @@ const getDogs = function(userInput) {
         .catch(error => console.log(error));
 };
 
+
+const handleSubmit = function() {
+    $('.dog-form').on('submit', event => {
+        event.preventDefault();
+        $('.dog-pics').empty();
+        let numOfDogs = $('#num-dogs').val();
+        getDogs(numOfDogs);
+    });
+};
+
+const handleBreedSearch = function() {
+    //handle submit button
+    $('.breed-dog-form').on('submit', event=> {
+        event.preventDefault();
+        $('.dog-pics').empty();
+        getBreeds($('#breed-type').val());
+    })
+    
+}
+
 const appendDogs = function(dogData){
     dogData.message.forEach(data => {
         $('.dog-pics').append(createTemplate(data))
     });
 };
 
-const handleSubmit = function() {
-    $('.dog-form').on('submit', event => {
-        event.preventDefault();
-        getDogs($('#num-dogs').val());
-    });
-};
-
-const handleBreedSearch = function() {
-    //handle submit button
-    
-}
-
 const getBreeds = function(breedType){
     //handle api data
-}
+    fetch(`https://dog.ceo/api/breed/${breedType}/images/random`)
+        .then(response => response.json())
+        .then(jsonData => {
+            appendBreeds(jsonData)
+        })
+        .catch(error => console.log(error));
+};
 
 const appendBreeds = function(breedData){
     //append breed img
-}
+    $('.dog-pics').append(createTemplate(breedData.message));
+};
 
 const createTemplate = function(dogMessage){
     return `<img src=${dogMessage}>`;
